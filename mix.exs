34e44ee1760a2372a25796403c0b96ec06bb5ce0
Anurag Soni <anuragsoni.13@gmail.com>
2018-01-15 00:02:47 -0500

    relax elixir version in mix file

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule IdGen.MixProject do
     [
       app: :id_gen,
       version: "0.1.0",
-      elixir: "~> 1.6-rc",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
